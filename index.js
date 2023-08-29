b9465b5f29b30392df5b0b7d5396325c2e30fc41
phamkhoa <phamkhoa.dev1510@gmail.com>
2023-08-29 16:28:30 +0700

    4:30

---
+++ index.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById("moon_icoin");
+
+
+let changeIcon = function (icon) {
+  icon.classList.toggle("bi-sun");
+  
+};
+btn.onclick = function () {
+  const addClass = document.getElementById("darl_mode");
+  document.body.classList.toggle("dark_theme");
+};
+
+function darkTheme (){
+
+document.getElementById('site-header').classList.toggle('dark')
+}
