f1d9d2f776ba52c67ab4bad96dbc0130da54f14e
phamkhoa <phamkhoa.dev1510@gmail.com>
2023-08-30 21:46:09 +0700

    end

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
-function darkTheme (){
-
- var element = document.body;
-  element.classList.toggle("dark-mode");
-
-}
+document.getElementById('dark-mode-btn').addEventListener('click', function (e) {
+  const toggler = document.body;
+  toggler.classList.toggle('dark-mode');
+  const target = e.target;
+  target.classList.toggle('fa-moon');
+  target.classList.toggle('fa-sun');
+});
